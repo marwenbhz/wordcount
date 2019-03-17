ae5c7b70bf50b0f5149ed97ce91e7977669fa0dc
Marwen Ben Hriz <marwen.benhriz@gmail.com>
2019-03-17 18:53:53 +0100

    Update views.py

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,6 +1,5 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import mysql.connector as mariadb
 import requests
 
 # Create your views here.
@@ -19,14 +18,6 @@ def wordcount(request, format=None):
         r = requests.get(url)
         if r.status_code == 200:
             count = r.text.count(word)
-            '''
-            # Store to DB.
-            mariadb_connection = mariadb.connect(user='root', password='09071992ma', database='wordcount')
-            cursor = mariadb_connection.cursor()
-            cursor.execute("INSERT INTO wordcount (url,word,status,count) VALUES (%s,%s, %s, %s)", (url,word,status,count))
-            mariadb_connection.commit()
-            mariadb_connection.close()
-            '''
             return Response({"status": r.status_code, "count": count})                
         else:
             return Response({"status": r.status_code, "message": "Error in url"})
